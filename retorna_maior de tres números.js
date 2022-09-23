0168842687fd2ae43b13dd14c19e0d9f79470800
Wlauber <wlaubersamuel@gmail.com>
2022-09-23 18:32:23 -0300

    exercicio retorna maior de tres numeros

---
+++ retorna_maior de tres números.js
@@ -0,0 +1,74 @@
+const a = 15
+const b = 33
+const c = 45
+
+if (a > b && a > c){
+    console.log ("O maior número é: " + (a))
+} else if (b > a && b > c){
+    console.log("O maior némero é: " + (b));
+}else{
+    console.log("O maior número é:" + (c));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const a = 6;
+// const b = 4;
+// const c = 2;
+
+// if (a > b && a > c) {
+//   console.log('O maior número é: ' + a + ' (a)');
+// } else if (b > a && b > c) {
+//   console.log('O maior número é: ' + b + ' (b)');
+// } else {
+//   console.log('O maior número é: ' + c + ' (c)');
+// };
